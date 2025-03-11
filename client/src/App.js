5a28652cee4199153755fa15675dfd92a469ed0b
NOVA-QYT <820043860@qq.com>
2025-03-11 18:06:54 -0400

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React, from Y
+            Learn React, from YQ
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
